e7bba3edd5f764268f2788e03cc219d0009768fc
Lucas Levi <lucas.lsl12@gmail.com>
2021-10-18 16:35:02 -0300

    spring structure

---
+++ src/test/java/com/lucaslevi/citiesapi/CitiesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaslevi.citiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
