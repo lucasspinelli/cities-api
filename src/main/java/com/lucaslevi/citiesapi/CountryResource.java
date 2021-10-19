9369b53b31faef385fdedf8482fbd0237b203532
Lucas Levi <lucas.lsl12@gmail.com>
2021-10-18 23:03:01 -0300

    find by id

--- src/main/java/com/lucaslevi/citiesapi/CountryResource.java
+++ src/main/java/com/lucaslevi/citiesapi/CountryResource.java
@@ -5,10 +5,13 @@ import com.lucaslevi.citiesapi.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
 
 
 @RestController
@@ -24,5 +27,13 @@ public class CountryResource {
     @GetMapping
     public Page<Country> countries(Pageable page){
         return repository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getOne(@PathVariable Long id){
+        Optional<Country> optional = repository.findById(id);
+
+        return optional.isPresent() ? ResponseEntity.ok().body(optional.get()) : ResponseEntity.notFound().build();
+
     }
 }
