00dad661881164657069b68ef659e4297b820b0e
Lucas Levi <lucas.lsl12@gmail.com>
2021-10-19 13:27:58 -0300

    add distance service

---
+++ src/main/java/com/lucaslevi/citiesapi/distances/DistanceResource.java
@@ -0,0 +1,38 @@
+package com.lucaslevi.citiesapi.distances;
+
+import org.apache.juli.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/distances")
+public class DistanceResource {
+
+    Logger log = LoggerFactory.getLogger(DistanceResource.class);
+
+    private final DistanceService service;
+
+    public DistanceResource (DistanceService service){
+        this.service = service;
+    }
+
+    @GetMapping("/by-points")
+    public Double byPoints(@RequestParam (name = "from") final Long cityA,
+                           @RequestParam(name= "to") final Long cityB){
+        log.info("byPoints");
+        return service.distanceByPointsInMiles(cityA, cityB);
+    }
+
+    @GetMapping("/by-cube")
+    public Double byCube(@RequestParam (name = "from") final Long cityA,
+                         @RequestParam(name= "to") final Long cityB){
+        log.info("byCube");
+        return service.distanceByCubeInMeters(cityA, cityB);
+    }
+
+
+}
