5091bb66ed9556eee4402f4732f16dd1ab745ef0
Lucas Levi <lucas.lsl12@gmail.com>
2021-10-18 22:50:27 -0300

    adding pageable

--- src/main/java/com/lucaslevi/citiesapi/CountryResource.java
+++ src/main/java/com/lucaslevi/citiesapi/CountryResource.java
@@ -3,11 +3,13 @@ package com.lucaslevi.citiesapi;
 import com.lucaslevi.citiesapi.countries.Country;
 import com.lucaslevi.citiesapi.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/countries")
@@ -19,8 +21,8 @@ public class CountryResource {
         this.repository = repository;
     }
 
-    @GetMapping()
-    public List<Country> countries(){
-        return repository.findAll();
+    @GetMapping
+    public Page<Country> countries(Pageable page){
+        return repository.findAll(page);
     }
 }
